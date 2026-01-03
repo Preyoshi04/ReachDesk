4877b254572ff47bbc88458183c0886055a562b9
PREYOSHI MONDAL <preyoshimondal@gmail.com>
2026-01-03 11:12:39 +0530

    backend url edited

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "https://reachdesk.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://reachdesk-backend.onrender.com";
 export default BASE_URL;
